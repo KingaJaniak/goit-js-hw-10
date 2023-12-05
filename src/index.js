02318d70eef56eabdee7451dbf274f8fadad1efd
Kinga Janiak <janiak.kinga96@gmail.com>
2023-12-05 18:18:43 +0100

    7

--- src/index.js
+++ src/index.js
@@ -1,47 +1,52 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
-import { fetchBreeds, fetchCatByBreed } from './cat-api'; 
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const catInfo = document.querySelector('.cat-info');
+const init = true;
 
 // hide elements
+hideElement(breedSelect);
 hideElement(catInfo);
-showLoader().then(() => {
-  showElement(breedSelect)
+showLoader(true).then(() => {
+  showElement(breedSelect);
 });
-
 fetchBreeds()
-.then(data => {
-  const html = data.map(breed => `<option value="${breed.id}">${breed.name}</option>`).join('');
-  breedSelect.innerHTML = html;
-  // hide loader
-  hideElement(loader);
-}). catch(error => {
-  handleRequestError(error);
-});
+  .then(data => {
+    const html = data
+      .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
+      .join('');
+    breedSelect.innerHTML = html;
+    showElement(breedSelect); // hide loader
+    hideElement(loader);
+  })
+  .catch(error => {
+    handleRequestError(error);
+  });
 
 breedSelect.addEventListener('change', handleBreedSelectChange);
 
-function showLoader() {
-  return new Promise((resolve) => {
-  showElement(loader);
-  hideElement(catInfo);
-  hideElement(breedSelect);
-  hideElement(document.querySelector('.breed-select'));
-  showElement(document.querySelector('.loader'));
-  setTimeout(()=>{
-    resolve()}, 500);
+function showLoader(init) {
+  return new Promise(resolve => {
+    showElement(loader);
+    hideElement(catInfo);
+    if (init) {
+      hideElement(breedSelect);
+    }
+    setTimeout(() => {
+      resolve();
+    }, 500);
   });
-};
-function hideLoader() {
+}
+function hideLoader(init) {
   hideElement(loader);
   showElement(catInfo);
-  showElement(breedSelect);
-  showElement(document.querySelector('.breed-select'));
-  hideElement(document.querySelector('.loader'));
-};
+  if (init) {
+    showElement(breedSelect);
+  }
+}
 
 function handleBreedSelectChange() {
   const selectedBreedId = breedSelect.value;
@@ -72,25 +77,27 @@ function handleBreedSelectChange() {
       handleRequestError(error);
     })
     .finally(() => {
-      // hide loader 
+      // hide loader
       hideLoader();
     });
-};
+}
 
 function resetUIElements() {
   hideElement(loader);
   hideElement(catInfo);
-};
+}
 
 function showElement(element) {
   element.classList.remove('is-hidden');
-};
+}
 
 function hideElement(element) {
   element.classList.add('is-hidden');
-};
+}
 
 function handleRequestError(error) {
-  Notiflix.Notify.failure('Oops! Something went wrong. Please try again later.');
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong. Please try again later.'
+  );
   console.error(error);
-};
+}
